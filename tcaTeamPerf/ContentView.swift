e30f90d8d5a775b192c75462579ac78afca4fbde
wingwarp <jtmorris@earthlink.net>
2020-10-16 11:47:28 -0700

    Initial Commit

---
+++ tcaTeamPerf/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tcaTeamPerf
+//
+//  Created by Jim Morris on 10/16/20.
+//  Copyright © 2020 Jim Morris. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
